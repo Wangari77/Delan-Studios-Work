ebf3b902a6bf3d5c797503e4c0fe483b66151468
Wangari77 <phyllismwangi7@gmail.com>
2019-09-29 14:12:24 +0300

    Added toggle effect to all the icons

--- js/scripts.js
+++ js/scripts.js
@@ -3,5 +3,15 @@ $(document).ready(function() {
       $("#peach").toggle();
       $("#click1").toggle();
     });
+
+    $(".onClick2").click(function() {
+      $("#peach1").toggle();
+      $("#click2").toggle();
+    });
+
+    $(".onClick3").click(function() {
+      $("#peach2").toggle();
+      $("#click3").toggle();
+    });
   });
-      
+ 
